ca75d5361070e674f215a8f1baac67c55cdb78ef
yy406961 <406961038@qq.com>
2020-04-01 17:22:29 +0800

    add province

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const qinghai = require('./province/qinghai')
 const ningxia = require('./province/ningxia')
 const jilin = require('./province/jilin')
-const shanxi = require('./province/shanxi')
+const shanxi = require('./province/shanxi') // 山西
+const shanxi2 = require('./province/shanxi2') // 陕西
 const haikou = require('./province/haikou')
 const changsha = require( './province/changsha')
 const jiangsu = require('./province/jiangsu')
@@ -9,17 +10,68 @@ const changping = require('./province/changping')
 const jiangxi = require('./province/jiangxi')
 const hunan = require('./province/hunan')
 const shunyi = require('./province/shunyi')
+const taiwan = require('./province/taiwan')
+const hebei = require('./province/hebei')
+const neimenggu = require('./province/neimenggu')
+const liaoning = require('./province/liaoning')
+const heilongjiang = require('./province/heilongjiang')
+const zhejiang = require('./province/zhejiang')
+const anhui = require('./province/anhui')
+const fujian = require('./province/fujian')
+const shandong = require('./province/shandong')
+const henan = require('./province/henan')
+const hubei = require('./province/hubei')
+const guangxi = require('./province/guangxi')
+const hainan = require('./province/hainan')
+const sichuan = require('./province/sichuan')
+const guizhou = require('./province/guizhou')
+const yunnan = require('./province/yunnan')
+const xizang = require('./province/xizang')
+const gansu = require('./province/gansu')
+const xinjiang = require('./province/xinjiang')
+const shanghai = require('./province/shanghai')
+const chongqing = require('./province/chongqing')
+const xianggang = require('./province/xianggang')
+const aomen = require('./province/aomen')
+const beijing = require('./province/beijing')
+const tianjing = require('./province/tianjing')
 
 module.exports = {
     qinghai,
     ningxia,
     jilin,
     shanxi,
+    shanxi2,
     haikou,
     changsha,
     jiangsu,
     changping,
     jiangxi,
     hunan,
-    shunyi
+    shunyi,
+    taiwan,
+    hebei,
+    neimenggu,
+    liaoning,
+    heilongjiang,
+    zhejiang,
+    anhui,
+    fujian,
+    shandong,
+    henan,
+    hubei,
+    guangxi,
+    hainan,
+    sichuan,
+    guizhou,
+    yunnan,
+    xizang,
+    gansu,
+    xinjiang,
+    shanghai,
+    chongqing,
+    xianggang,
+    aomen,
+    beijing,
+    tianjing
 }
